f564007d90b32c74a2e90f8ffb4b520bf3e10428
Yaman-dot <56838069+Yaman-dot@users.noreply.github.com>
2025-06-02 03:01:39 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,8 @@
-/** @type {import('next').NextConfig} */
+// next.config.mjs
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  output: "export",  // <=== enables static exports
+  basePath: "/PortfolioWebsite",
+  output: "export",
   reactStrictMode: true,
-}
+};
 
-<<<<<<< HEAD
-export default nextConfig
-=======
 export default nextConfig;
->>>>>>> 9c818926bb331136c15fb39bc9143ffc74cbd1f3
